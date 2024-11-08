fb67e974ac5479327f9f33fde6aeb5e5b7a36337
Francisco Torres Pérez <franute@gmail.com>
2024-11-08 23:27:42 +0100

    Disable/enable systemd units by default

--- build.sh
+++ build.sh
@@ -21,3 +21,5 @@ rpm-ostree install screen
 #### Example for enabling a System Unit File
 
 systemctl enable podman.socket
+systemctl disable flatpak-add-fedora-repos
+systemctl disable NetworkManager-wait-online
