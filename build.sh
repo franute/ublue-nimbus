1614c6ca9379954b9d293c84ef7291c692ae75ca
Francisco Torres Pérez <franute@gmail.com>
2024-11-08 23:40:28 +0100

    - Added some default packages to install - Remove some base packages - Add atim's starship and ubuntu-fonts repositories

--- build.sh
+++ build.sh
@@ -4,6 +4,10 @@ set -ouex pipefail
 
 RELEASE="$(rpm -E %fedora)"
 
+### Setup repositories ###
+
+curl -Lo /etc/yum.repos.d/atim-starship-fedora-"${RELEASE}".repo https://copr.fedorainfracloud.org/coprs/atim/starship/repo/fedora-"${RELEASE}"/atim-starship-fedora-"${RELEASE}".repo
+curl -Lo /etc/yum.repos.d/atim-ubuntu-fonts-fedora-"${RELEASE}".repo https://copr.fedorainfracloud.org/coprs/atim/ubuntu-fonts/repo/fedora-"${RELEASE}"/atim-ubuntu-fonts-fedora-"${RELEASE}".repo
 
 ### Install packages
 
@@ -13,7 +17,13 @@ RELEASE="$(rpm -E %fedora)"
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-rpm-ostree install screen
+# rpm-ostree install screen
+rpm-ostree install evince simple-scan ubuntu-family-fonts \
+            fira-code-fonts cascadia-code-nf-fonts \
+            java-17-openjdk
+
+rpm-ostree override remove virtualbox-guest-additions \
+            gnome-shell-extension-background-logo
 
 # this would install a package from rpmfusion
 # rpm-ostree install vlc
@@ -23,3 +33,15 @@ rpm-ostree install screen
 systemctl enable podman.socket
 systemctl disable flatpak-add-fedora-repos
 systemctl disable NetworkManager-wait-online
+
+### Cleanup ###
+
+# Remove unnecessary packages
+rpm-ostree cleanup -m
+
+# Remove temporary files and caches
+rm -rf /var/cache/dnf /var/lib/dnf /tmp/* /var/tmp/*
+
+# Disable newly added repositories
+sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/atim-starship-fedora-"${RELEASE}".repo
+sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/atim-ubuntu-fonts-fedora-"${RELEASE}".repo
